6f83d4058a193965495da4af6187dbbbdb78ad0e
Jagadesh Babu Thangavelu <jagadeshbabut@gmail.com>
2021-12-28 14:43:32 +0530

    Add other chat tools

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,19 @@
 import '../styles/globals.css'
+import Script from "next/script";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+
+  return (
+    <>
+        <Script
+          type='text/javascript'
+          id="ze-snippet"
+          src="https://static.zdassets.com/ekr/snippet.js?key=e10d156e-226c-44e5-9815-d4c28ddbe458"
+          strategy='beforeInteractive'
+        />
+        <Component {...pageProps} />
+    </>
+  );
 }
 
 export default MyApp
